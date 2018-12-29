044b02f71b9208fd8091f5658b7af1ccdd895754
dongmy54 <615760433@qq.com>
2018-12-29 14:59:37 +0800

    finish

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "chat_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_cable.url = 'wss://peaceful-basin-31856.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [
+    'https://peaceful-basin-31856.herokuapp.com' ]
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
